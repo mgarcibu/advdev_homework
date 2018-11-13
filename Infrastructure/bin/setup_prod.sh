93cc1edf2ccea8c4c7a39e25e8dbf10bdbcf3028
mgarcibu <41591471+mgarcibu@users.noreply.github.com>
2018-11-13 12:22:35 +0100

    Update setup_prod.sh

--- Infrastructure/bin/setup_prod.sh
+++ Infrastructure/bin/setup_prod.sh
@@ -12,4 +12,4 @@ echo "Setting up Parks Production Environment in project ${GUID}-parks-prod"
 # Code to set up the parks production project. It will need a StatefulSet MongoDB, and two applications each (Blue/Green) for NationalParks, MLBParks and Parksmap.
 # The Green services/routes need to be active initially to guarantee a successful grading pipeline run.
 
-# To be Implemented by Student
+. ../templates/prod.sh
