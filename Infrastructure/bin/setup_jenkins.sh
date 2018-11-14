6d12bc42fcc01cc5d2c4dd695ba8d73dcaf7b2bd
mgarcibu <41591471+mgarcibu@users.noreply.github.com>
2018-11-14 12:28:32 +0100

    Update setup_jenkins.sh

--- Infrastructure/bin/setup_jenkins.sh
+++ Infrastructure/bin/setup_jenkins.sh
@@ -26,4 +26,4 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 # * GUID: the GUID used in all the projects
 # * CLUSTER: the base url of the cluster used (e.g. na39.openshift.opentlc.com)
 
-. ../templates/jenkins.sh
+oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi -n $GUID-jenkins
