e05f79ec724ca974ab98fb640229ba00eada0fa3
mgarcibu <41591471+mgarcibu@users.noreply.github.com>
2018-11-14 21:27:23 +0100

    Update setup_dev.sh

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -11,34 +11,106 @@ echo "Setting up Parks Development Environment in project ${GUID}-parks-dev"
 
 # Code to set up the parks development project.
 
-oc project $GUID-sonarqube
+oc project $GUID-parks-dev
 
-oc new-app --template=postgresql-persistent --param POSTGRESQL_USER=sonar --param POSTGRESQL_PASSWORD=sonar --param POSTGRESQL_DATABASE=sonar --param VOLUME_CAPACITY=4Gi --labels=app=sonarqube_db
+oc policy add-role-to-user edit system:serviceaccount:f9ff-jenkins:jenkins -n f9ff-parks-dev
 
-oc new-app --docker-image=wkulhanek/sonarqube:6.7.4 --env=SONARQUBE_JDBC_USERNAME=sonar --env=SONARQUBE_JDBC_PASSWORD=sonar --env=SONARQUBE_JDBC_URL=jdbc:postgresql://postgresql/sonar --labels=app=sonarqube
-
-oc new-app wkulhanek/sonarqube:6.7.4 --env=SONARQUBE_JDBC_USERNAME=sonar --env=SONARQUBE_JDBC_PASSWORD=sonar --env=SONARQUBE_JDBC_URL=jdbc:postgresql://postgresql/sonar --labels=app=sonarqube
-
-oc rollout pause dc sonarqube
-oc expose svc sonarqube
-
-echo "apiVersion: v1
-kind: PersistentVolumeClaim
+echo 'kind: StatefulSet
+apiVersion: apps/v1
 metadata:
-  name: sonarqube-pvc
+  name: "mongodb"
 spec:
-  accessModes:
-  - ReadWriteOnce
-  resources:
-    requests:
-      storage: 4Gi" | oc create -f -
+  serviceName: "mongodb-internal"
+  replicas: 1
+  selector:
+    matchLabels:
+      name: mongodb
+  template:
+    metadata:
+      labels:
+        name: "mongodb"
+    spec:
+      containers:
+        - name: mongo-container
+          image: "registry.access.redhat.com/rhscl/mongodb-34-rhel7:latest"
+          ports:
+            - containerPort: 27017
+          args:
+            - "run-mongod-replication"
+          volumeMounts:
+            - name: mongo-data
+              mountPath: "/var/lib/mongodb/data"
+          env:
+            - name: MONGODB_DATABASE
+              value: "mongodb"
+            - name: MONGODB_USER
+              value: "mongodb_user"
+            - name: MONGODB_PASSWORD
+              value: "mongodb_password"
+            - name: MONGODB_ADMIN_PASSWORD
+              value: "mongodb_admin_password"
+            - name: MONGODB_REPLICA_NAME
+              value: "rs0"
+            - name: MONGODB_KEYFILE_VALUE
+              value: "12345678901234567890"
+            - name: MONGODB_SERVICE_NAME
+              value: "mongodb-internal"
+          readinessProbe:
+            exec:
+              command:
+                - stat
+                - /tmp/initialized
+  volumeClaimTemplates:
+    - metadata:
+        name: mongo-data
+        labels:
+          name: "mongodb"
+      spec:
+        accessModes: [ ReadWriteOnce ]
+        resources:
+          requests:
+            storage: "4Gi"' | oc create -f -
 
-oc set volume dc sonarqube --add --overwrite --name=sonarqube-volume-1 --mount-path=/opt/sonarqube/data/ --type persistentVolumeClaim --claim-name=sonarqube-pvc
+oc new-build --binary=true --name=mlbparks jboss-eap70-openshift:1.7 -n f9ff-parks-dev
+oc new-build --binary=true --name=nationalparks redhat-openjdk18-openshift:1.2 -n f9ff-parks-dev
+oc new-build --binary=true --name=parksmap redhat-openjdk18-openshift:1.2 -n f9ff-parks-dev
 
-oc set resources dc sonarqube --limits=memory=3Gi,cpu=2 --requests=memory=2Gi,cpu=1
-oc patch dc sonarqube --patch='{ "spec": { "strategy": { "type": "Recreate" }}}'
+oc new-app f9ff-parks-dev/mlbparks:0.0-0 --allow-missing-imagestream-tags=true --name=mlbparks -l type=parksmap-backend --allow-missing-imagestream-tags=true -n f9ff-parks-dev
+oc new-app f9ff-parks-dev/nationalparks:0.0-0 --allow-missing-imagestream-tags=true --name=national-parks-dev -l type=parksmap-backend --allow-missing-imagestream-tags=true -n f9ff-parks-dev
+oc new-app f9ff-parks-dev/parksmap:0.0-0 --allow-missing-imagestream-tags=true --name=parksmap-dev -l type=parksmap-frontend --allow-missing-imagestream-tags=true -n f9ff-parks-dev
 
-oc set probe dc/sonarqube --liveness --failure-threshold 3 --initial-delay-seconds 40 -- echo ok
-oc set probe dc/sonarqube --readiness --failure-threshold 3 --initial-delay-seconds 20 --get-url=http://:9000/about
+oc set triggers dc mlbparks --remove-all -n f9ff-parks-dev
+oc set triggers dc nationalparks --remove-all -n f9ff-parks-dev
+oc set triggers dc parksmap --remove-all -n f9ff-parks-dev
 
-oc rollout resume dc sonarqube
+oc expose dc mlbparks --port 8080 -n f9ff-parks-dev
+oc expose dc nationalparks --port 8080 -n f9ff-parks-dev
+oc expose dc parksmap --port 8080 -n f9ff-parks-dev
+oc expose svc/parksmap -n ${GUID}-parks-dev
+
+oc set probe dc/mlbparks --readiness --initial-delay-seconds 30 --failure-threshold 3 --get-url=http://:8080/ws/healthz/ -n ${GUID}-parks-dev
+oc set probe dc/mlbparks --liveness --initial-delay-seconds 30 --failure-threshold 3 --get-url=http://:8080/ws/healthz/ -n ${GUID}-parks-dev
+oc set probe dc/nationalparks --readiness --initial-delay-seconds 30 --failure-threshold 3 --get-url=http://:8080/ws/healthz/ -n ${GUID}-parks-dev
+oc set probe dc/nationalparks --liveness --initial-delay-seconds 30 --failure-threshold 3 --get-url=http://:8080/ws/healthz/ -n ${GUID}-parks-dev
+oc set probe dc/parksmap --readiness --initial-delay-seconds 30 --failure-threshold 3 --get-url=http://:8080/ws/healthz/ -n ${GUID}-parks-dev
+oc set probe dc/parksmap --liveness --initial-delay-seconds 30 --failure-threshold 3 --get-url=http://:8080/ws/healthz/ -n ${GUID}-parks-dev
+
+oc create configmap parksdb-conf -n ${GUID}-parks-dev \
+       --from-literal=DB_HOST=mongodb \
+       --from-literal=DB_PORT=27017 \
+       --from-literal=DB_USERNAME=mongodb_user \
+       --from-literal=DB_PASSWORD=mongodb_password \
+       --from-literal=DB_NAME=mongodb-internal
+oc create configmap mlbparks-config --from-literal=APPNAME="MLB Parks (Dev)" -n f9ff-parks-dev
+oc create configmap nationalparks-config --from-literal=APPNAME="National Parks (Dev)" -n f9ff-parks-dev
+oc create configmap parksmap-config --from-literal=APPNAME="ParksMap (Dev)" -n f9ff-parks-dev
+
+oc set env dc/mlbparks --from=configmap/parksdb-conf -n ${GUID}-parks-dev
+oc set env dc/mlbparks --from=configmap/nationalparks-conf -n ${GUID}-parks-dev
+oc set env dc/nationalparks --from=configmap/parksdb-conf -n ${GUID}-parks-dev
+oc set env dc/nationalparks --from=configmap/nationalparks-conf -n ${GUID}-parks-dev
+oc set env dc/parksmap --from=configmap/parksdb-conf -n ${GUID}-parks-dev
+oc set env dc/parksmap --from=configmap/nationalparks-conf -n ${GUID}-parks-dev
+
+oc set deployment-hook dc/mlbparks --post -- curl -s http://mlbparks:8080/ws/data/load/ -n ${GUID}-parks-dev
+oc set deployment-hook dc/nationalparks --post -- curl -s http://nationalparks:8080/ws/data/load/ -n ${GUID}-parks-dev
